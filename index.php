9b1b01cf7de0600c1e0a6aeedc3ae986099004ed
stephbak <stephane.bakum@sfr.fr>
2019-01-10 09:02:02 +0100

    p7 exe1

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+<head>
+  <meta charset="utf-8" />
+  <link rel="stylesheet" href="style.css" />
+  <title>php partie7</title>
+</head>
+<body>
+  <form  action="user.php" method="GET">
+    <label for="firstname">Prénom : <input id="firstname" type="text" name="firstname" /></label>
+    <label for="lastname">Nom : <input id="lastname" type="text" name="lastname" /></label>
+    <input type="submit" name="submit" value="sauvegarder" />
+  </form>
+</body>
+</html>
